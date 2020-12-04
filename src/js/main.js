cdfec5304cbbaa508d29626b488d72aea2e19c78
Georgy <thesuperboy112@gmail.com>
2020-12-04 13:33:29 +0300

    one shot

--- src/js/main.js
+++ src/js/main.js
@@ -9,8 +9,9 @@ height: screen.height,
 backgroundColor: 0x0033ff,
 
 view: document.querySelector('#scene'),
-resolution: window.devicePixelRatio || 1
+resolution: window.devicePixelRatio || 1,
 });
+document.body.appendChild(app.view);
 
 const loader = PIXI.Loader.shared,
     title = document.title,
@@ -34,6 +35,160 @@ loader.on('progress',(loader,resoure)=>{
   document.title = Math.round(loader.progress);
 }).load((progress,resources)=>{
 document.title = title;
+
+// Options for how objects interact
+// How fast the red square moves
+const movementSpeed = 0.05;
+
+// Strength of the impulse push between two objects
+const impulsePower = 5;
+
+// Test For Hit
+// A basic AABB check between two different squares
+function testForAABB(object1, object2) {
+    const bounds1 = object1.getBounds();
+    const bounds2 = object2.getBounds();
+
+    return bounds1.x < bounds2.x + bounds2.width
+        && bounds1.x + bounds1.width > bounds2.x
+        && bounds1.y < bounds2.y + bounds2.height
+        && bounds1.y + bounds1.height > bounds2.y;
+}
+
+// Calculates the results of a collision, allowing us to give an impulse that
+// shoves objects apart
+function collisionResponse(object1, object2) {
+    if (!object1 || !object2) {
+        return new PIXI.Point(0);
+    }
+
+    const vCollision = new PIXI.Point(
+        object2.x - object1.x,
+        object2.y - object1.y,
+    );
+
+    const distance = Math.sqrt(
+        (object2.x - object1.x) * (object2.x - object1.x)
+        + (object2.y - object1.y) * (object2.y - object1.y),
+    );
+
+    const vCollisionNorm = new PIXI.Point(
+        vCollision.x / distance,
+        vCollision.y / distance,
+    );
+
+    const vRelativeVelocity = new PIXI.Point(
+        object1.acceleration.x - object2.acceleration.x,
+        object1.acceleration.y - object2.acceleration.y,
+    );
+
+    const speed = vRelativeVelocity.x * vCollisionNorm.x
+        + vRelativeVelocity.y * vCollisionNorm.y;
+
+    const impulse = impulsePower * speed / (object1.mass + object2.mass);
+
+    return new PIXI.Point(
+        impulse * vCollisionNorm.x,
+        impulse * vCollisionNorm.y,
+    );
+}
+
+// Calculate the distance between two given points
+function distanceBetweenTwoPoints(p1, p2) {
+    const a = p1.x - p2.x;
+    const b = p1.y - p2.y;
+
+    return Math.hypot(a, b);
+}
+
+// // The green square we will knock about
+// const greenSquare = new PIXI.Sprite(PIXI.Texture.WHITE);
+// greenSquare.position.set((app.screen.width - 100) / 2, (app.screen.height - 100) / 2);
+// greenSquare.width = 100;
+// greenSquare.height = 100;
+// greenSquare.tint = '0x00FF00';
+// greenSquare.acceleration = new PIXI.Point(0);
+// greenSquare.mass = 3;
+
+// // The square you move around
+// const redSquare = new PIXI.Sprite(PIXI.Texture.WHITE);
+// redSquare.position.set(0, 0);
+// redSquare.width = 100;
+// redSquare.height = 100;
+// redSquare.tint = '0xFF0000';
+// redSquare.acceleration = new PIXI.Point(0);
+// redSquare.mass = 1;
+
+// Listen for animate update
+function Phisicks(redSquare, greenSquare){}
+//     // Applied deacceleration for both squares, done by reducing the
+//     // acceleration by 0.01% of the acceleration every loop
+//     redSquare.acceleration.set(redSquare.acceleration.x * 0.99, redSquare.acceleration.y * 0.99);
+//     greenSquare.acceleration.set(greenSquare.acceleration.x * 0.99, greenSquare.acceleration.y * 0.99);
+
+    
+
+//     // Check whether the green square ever moves off the screen
+//     // If so, reverse acceleration in that direction
+//     if (greenSquare.x < 0 || greenSquare.x > (app.screen.width - 100)) {
+//         greenSquare.acceleration.x = -greenSquare.acceleration.x;
+//     }
+
+//     if (greenSquare.y < 0 || greenSquare.y > (app.screen.height - 100)) {
+//         greenSquare.acceleration.y = -greenSquare.acceleration.y;
+//     }
+
+//     // If the green square pops out of the cordon, it pops back into the
+//     // middle
+//     if ((greenSquare.x < -30 || greenSquare.x > (app.screen.width + 30))
+//         || greenSquare.y < -30 || greenSquare.y > (app.screen.height + 30)) {
+//         greenSquare.position.set((app.screen.width - 100) / 2, (app.screen.height - 100) / 2);
+//     }
+
+
+//         // Figure out the speed the square should be travelling by, as a
+//         // function of how far away from the mouse pointer the red square is
+//         const distMouseRedSquare = distanceBetweenTwoPoints(
+//             mouseCoords,
+//             redSquareCenterPosition,
+//         );
+//         const redSpeed = distMouseRedSquare * movementSpeed;
+
+//         // Calculate the acceleration of the red square
+//         redSquare.acceleration.set(
+//             Math.cos(angleToMouse) * redSpeed,
+//             Math.sin(angleToMouse) * redSpeed,
+//         );
+//     }
+
+
+//     // If the two squares are colliding
+//     if (testForAABB(greenSquare, redSquare)) {
+//         // Calculate the changes in acceleration that should be made between
+//         // each square as a result of the collision
+//         const collisionPush = collisionResponse(greenSquare, redSquare);
+//         // Set the changes in acceleration for both squares
+//         redSquare.acceleration.set(
+//             (collisionPush.x * greenSquare.mass),
+//             (collisionPush.y * greenSquare.mass),
+//         );
+//         greenSquare.acceleration.set(
+//             -(collisionPush.x * redSquare.mass),
+//             -(collisionPush.y * redSquare.mass),
+//         );
+//     }
+
+//     greenSquare.x += greenSquare.acceleration.x * delta;
+//     greenSquare.y += greenSquare.acceleration.y * delta;
+
+//     redSquare.x += redSquare.acceleration.x * delta;
+//     redSquare.y += redSquare.acceleration.y * delta;
+// }
+// };
+
+// Общие проверки для всей игры
+let main_check = Boolean(false)
+let level = 1;
 
 // Меню выбора
 // Стиль текстов
@@ -106,6 +261,7 @@ function randomizeStar(star, initial) {
     star.y = Math.sin(deg) * distance;
 }
 
+
 // Создание ракеты
     window.rocket = new PixiApngAndGif(imgs.rocket,resources);
     let startRocket = window.rocket.sprite;
@@ -118,6 +274,7 @@ function randomizeStar(star, initial) {
 
 
 // Начать игру
+
 const startText = new PIXI.Text('Start', style);
 startText.anchor.set(0.5);
 startText.scale.set(2);
@@ -130,11 +287,13 @@ startText.buttonMode = true;
 startText.on('pointerdown', onClickstartText);
 app.stage.addChild(startText);
 function onClickstartText(){
+  main_check = true;
   startText.alpha = 0;
   optionText.alpha = 0;
   startRocket.alpha = 0;
   AlsoGame();
 }
+
 
 // Опции
 const optionText = new PIXI.Text('Options', style);
@@ -144,6 +303,19 @@ optionText.x = app.screen.width/2;
 optionText.y = app.screen.height/2;
 optionText.zIndex = 10;
 app.stage.addChild(optionText);
+
+// Анимация ракеты
+app.ticker.add(() =>{
+  startRocket.y -= Math.random() * 2;
+  startRocket.x += 0.5;
+
+  if(startRocket.y < -100)
+  {
+  startRocket.y = app.screen.height;
+  startRocket.x = Math.random() * app.screen.width;
+  }
+})
+
 
 // Анимация тикера для звезд
 app.ticker.add((delta) => {
@@ -157,7 +329,7 @@ app.ticker.add((delta) => {
       const z = star.z - cameraZ;
       star.sprite.x = star.x * (fov / z) * app.renderer.screen.width + app.renderer.screen.width / 2;
       star.sprite.y = star.y * (fov / z) * app.renderer.screen.width + app.renderer.screen.height / 2;
-      star.y += 0.01;
+      star.y += 0.01*level;
 
       // 
       // Рахмер звезд и их вращение
@@ -173,16 +345,6 @@ app.ticker.add((delta) => {
   }
 });
 
-app.ticker.add(() =>{
-  startRocket.y -= Math.random() * 2;
-  startRocket.x += 0.5;
-
-  if(startRocket.y < -100)
-  {
-  startRocket.y = app.screen.height;
-  startRocket.x = Math.random() * app.screen.width;
-  }
-})
 
 // Сама игра
 function AlsoGame(){
@@ -191,6 +353,8 @@ GameContainer.visible = true;
 app.stage.interactive = true;
 app.stage.addChild(GameContainer);
 GameContainer.off();
+
+
 
 // Создание метеоритов
 
@@ -203,9 +367,11 @@ const astrosFrames = [
 const astros = [];
 var check = new Boolean(false); 
 let lvltarget = 1000;
-let level = 1;
+
 var i = 1;  
-let timer = 5000;        
+let timer = 5000;
+if(main_check == true)
+{        
 function AstroLoop () {           
 setTimeout(function () {
     const asteroid = astrosFrames[i%4];
@@ -217,10 +383,14 @@ setTimeout(function () {
     astro.x = Math.random() * app.screen.width;
     astro.turningSpeed = Math.random() - 0.8;
     astro.y = 0;
+    astro.mass = Math.random()  * 3;
     astro.speed = 1 + Math.random() * level;
     astro.visible = false;
-    astros.push(astro);
-    GameContainer.addChild(astro);;          
+    if(main_check == true)
+    {
+    astros.push(astro);   
+    GameContainer.addChild(astro);
+    }
     i++;                   
     if (i < lvltarget) {                 
     AstroLoop();                            
@@ -228,6 +398,7 @@ setTimeout(function () {
   }, Math.random() * timer)
 }
 AstroLoop();
+}
 
 
 // Создание взрыва
@@ -239,7 +410,7 @@ const bullet = PIXI.Sprite.from(texture2);
 bullet.anchor.set(0.5);
 bullet.interactive = true;
 bullet.x = app.screen.width/2;
-bullet.y = app.screen.height-200;
+bullet.y = app.screen.height-0.2*app.screen.height;
 bullet.scale.set(1.4);
 GameContainer.addChild(bullet);
 
@@ -247,7 +418,9 @@ GameContainer.addChild(bullet);
 let status = Boolean(false);
 const gun = [];
 let magazine = 50;
-var i = 1;        
+var i = 1; 
+if(main_check == true)
+{       
 function GunLoop () {           
 setTimeout(function () { 
   
@@ -258,11 +431,14 @@ setTimeout(function () {
   bullet2.scale.set(0.01);
   bullet2.interactive = true;
   bullet2.x = app.screen.width/2;
-  bullet2.y = app.screen.height-200;
+  bullet2.y = app.screen.height-0.2*app.screen.height;
   bullet2.alpha = 0;
   if(gun.length < 10)
+  if(main_check == true)
+    {
   gun.push(bullet2)
-  GameContainer.addChild(bullet2);  
+  GameContainer.addChild(bullet2); 
+    }
   i++;                       
     if (i < magazine) {                 
     GunLoop();                           
@@ -270,6 +446,7 @@ setTimeout(function () {
   }, 2000)
 }
 GunLoop();
+}
 
 // Создание ракеты
 window.rocket = new PixiApngAndGif(imgs.rocket,resources);
@@ -300,7 +477,9 @@ boomcounter.push(boom);}
 // Созднание аптек
 const heals = [];
 let lvltarget2 = 1000;
-var i = 1;                    
+var i = 1;      
+if(main_check == true)
+{               
 function HealLoop () {           
 setTimeout(function () {    
     const healpoint = PIXI.Sprite.from('/assets/healpoint.png');
@@ -312,8 +491,11 @@ setTimeout(function () {
     healpoint.scale.set(0.3);
     healpoint.speed = 1 + Math.random() * 2;
     healpoint.visible = false;
+    if(main_check == true)
+    {
     heals.push(healpoint);
-    GameContainer.addChild(healpoint);;          
+    GameContainer.addChild(healpoint);;
+    }     
     i++;                      
     if (i < lvltarget2) {                 
     HealLoop();                            
@@ -321,11 +503,14 @@ setTimeout(function () {
   },  Math.random() * (40000 - 10000) + 20000)
 }
 HealLoop();
+}
 
 // Создание магазинов для пулемета
 const magazines = [];
 let lvltarget3 = 500;
-var i = 1;                    
+var i = 1;     
+if(main_check == true)
+{                
 function MagazineLoop () {           
 setTimeout(function () {    
     const magaz = PIXI.Sprite.from('/assets/gunbullet2.png');
@@ -337,8 +522,11 @@ setTimeout(function () {
     magaz.scale.set(0.3);
     magaz.speed = 1 + Math.random() * 2;
     magaz.visible = false;
+    if(main_check == true)
+    {
     magazines.push(magaz);
-    GameContainer.addChild(magaz);;          
+    GameContainer.addChild(magaz);
+    }          
     i++;                              
     if (i < lvltarget3) {                 
     MagazineLoop();                            
@@ -346,6 +534,8 @@ setTimeout(function () {
   }, Math.random() * (60000 - 10000) + 20000)
 }
 MagazineLoop();
+}
+
 
 // Текст в игре
 
@@ -383,11 +573,13 @@ const pointText = new PIXI.Text(pointtarget, style2);
 basicText.fill = ['#ffffff'];
 pointText.x = app.screen.width/7;
 pointText.y = app.screen.height/6;
+pointText.anchor.set(0.5);
 pointText.zIndex = 10;
 GameContainer.addChild(pointText);
 
 const pointText2 = new PIXI.Text('SCORE:', style2)
-pointText2.x = pointText.x - 150;
+pointText2.x = pointText.x - 0.04*app.screen.width;
+pointText2.anchor.set(0.5);
 pointText2.y = app.screen.height/6;
 pointText2.zIndex = 10;
 GameContainer.addChild(pointText2);
@@ -397,12 +589,14 @@ let life = 3;
 const lifeText = new PIXI.Text(life, style2);
 lifeText.x = app.screen.width/7;
 lifeText.y = app.screen.height/5;
+lifeText.anchor.set(0.5);
 lifeText.zIndex = 10;
 GameContainer.addChild(lifeText);
 
 const lifeText2 = new PIXI.Text('TOTAL LIFES:', style2)
-lifeText2.x = lifeText.x - 260;
+lifeText2.x = lifeText.x - 0.06*app.screen.width;
 lifeText2.y = app.screen.height/5;
+lifeText2.anchor.set(0.5);
 lifeText2.zIndex = 10;
 GameContainer.addChild(lifeText2);
 
@@ -427,11 +621,44 @@ lvlText2.alpha = Math.sin(0);
 lvlText2.zIndex = 10;
 GameContainer.addChild(lvlText2);
 
+// Кнопка паузы
+let Pause_button = new PIXI.Sprite.from('assets/pause_button.png')
+GameContainer.addChild(Pause_button);
+Pause_button.scale.set(0.2);
+Pause_button.anchor.set(0.5);
+Pause_button.alpha = 1;
+Pause_button.x = app.screen.width/1.1;
+Pause_button.y = app.screen.height/5;
+Pause_button.interactive = true;
+Pause_button.buttonMode = true;
+Pause_button.on('pointerdown', onClickstartPause);
+
+
+let check_pause = Boolean(true);
+
+
+function onClickstartPause(){
+  if(check_pause == true){
+  app.ticker.stop();
+  check_pause = false;
+  main_check = false;
+  console.log("Status",status, "Размер массива обоймы",gun.length,'общая проверка', main_check, 'размер массива астеройдов', astros.length)
+  console.log('элемент массива взрыва',m,'булевая',check_boom)
+  console.log('таймер',timer);
+
+  }
+  else{
+  app.ticker.start();
+  check_pause = true;
+  main_check = true;
+  }
+}
+
 // Шкала
 
 let ScaleContainer = new PIXI.Container();
 GameContainer.addChild(ScaleContainer);
-ScaleContainer.x = lifeText.x - 260;;
+ScaleContainer.x = lifeText.x - 0.26*app.screen.width;;
 ScaleContainer.y = app.screen.height/1.2;
 
 let ScaleGunMass = [];
@@ -459,7 +686,6 @@ ScaleGun.alpha = 0;
       ScaleGun.alpha = 0; ScaleGun.width = 900;}},10000)
 }
   
-  
 // Функция обновления тикера сообщения
 function updateStatusMessage(Sometext, sometarget) {
   Sometext.text = sometarget;
@@ -480,11 +706,11 @@ if(pointtarget == level*10 + Math.floor(scale))
   setTimeout(() => {
     lvlText.alpha = 0;
     lvlText2.alpha = 0; 
-    if(timer > 400){timer -= 450 }},3000)
+    if(timer > 1500){timer -= 450 }},3000)
 } 
 if(level > 2)
 scale = scale + 0.0005;
-console.log('таймер',timer);
+
 })
 // Тикер анимации ракеты и ее пули
 app.ticker.add(() => {
@@ -512,8 +738,8 @@ if(life > 0){
   if(bullet.y !=0){
     bullet.y -= 10;
     }
-    else if(bullet.y == 0){
-    bullet.y = app.screen.height-200;
+  if(bullet.y < 0){
+    bullet.y = app.screen.height-0.2*app.screen.height;
     }
 })
 
@@ -523,9 +749,11 @@ app.ticker.add(() => {
     
     for (let i = 0; i < astros.length; i++) {
     const astro = astros[i];
+    const astro2 = astros[i+1]
+    Phisicks(astro,astro2)
     let Time = i + Math.random()*400;
     setTimeout(() => meteor_coming(astro, astros),Time);} 
-    console.log("Status",status, "Размер массива",gun.length)
+    
 });
 
 // Тикер падения аптечек
@@ -604,11 +832,20 @@ if(life < 0)
   boom.visible = true;
   bullet.y = 0;
   bullet.alpha = 0;
+  life = 0;
   updateStatusMessage(lifeText, 0);
   setTimeout(() => {
     boom.visible = false;
   }, 800);
-  life = 0;
+  
+  setTimeout(() =>{
+  app.stage.removeChild(GameContainer);
+  // GameContainer.destroy();
+  startText.alpha = 1;
+  optionText.alpha = 1;
+  startRocket.alpha = 1;
+  main_check = false;
+},5000)
 }
 else if(react(a, bunny) && life > 0 && a.interactive == true)
   { 
@@ -626,9 +863,10 @@ if (react(a, bullet) && a.interactive == true){
   boom.y = a.y
   boom.visible = true;
   setTimeout(() => visiblity(a),500);
-  b.splice(i);
   m++;
-  console.log('элемент массива взрыва',m,'булевая',check_boom)
+  if(b.length > 10)
+  b.splice(0, 1);
+  
   setTimeout(() => {
     boom.visible = false;
   }, 500);
@@ -639,13 +877,13 @@ if (delta == 0)
 if(m == boomcounter.length-1)
 m = 0;
 }
-if(a.y == bunny.y + 50)
+if(a.y == app.screen.height - 100)
 {
   a.interactive = false;
   app.stage.ticker.stop(a);
   GameContainer.removeChild(a);
   a.destroy();
-  b.splice(i);
+  b.splice(0, 1);
 }
 
 // Реакция пулемета на астеройды
@@ -665,6 +903,8 @@ if(a.y == bunny.y + 50)
     a.rotation -= Math.random() * 0.05;
     a.scale.set(0.1);
     a.tint = 0xFFFFFF;
+    if(b.length > 10)
+    b.splice(0, 1);
     setTimeout(() => visiblity(a),200);}
   if(status == true){
   bullet2.alpha = 1;  
@@ -699,7 +939,7 @@ function heal_coming(b){
 {
   app.stage.ticker.stop(b);
   b.destroy;
-  b.splice(i);
+  b.splice(0, 1);
 }
 }
 
